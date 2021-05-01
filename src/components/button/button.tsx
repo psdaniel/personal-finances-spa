34270d0405c54348ca416422995b5324297c176f
Daniel <daniel.n10@hotmail.com>
2021-05-01 11:50:51 -0300

    Define global styles

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,12 +3,30 @@ import {makeStyles} from '@material-ui/core';
 import {StylesContext} from '@material-ui/styles';
 
 export const Button = () => {
-    const useStyles = makeStyles({
-        buttonStyles: {
-            backgroundColor: '#FECB00',
-            color: '#181818'
+    const useStyles = makeStyles(() => ({
+        buttonStyles: () => {
+            const buttonWidht = 150;
+
+            return {
+                backgroundColor: '#FECB00',
+                color: '#181818',
+                fontSize: '1.2rem',
+                padding: '1rem',
+                width: buttonWidht,
+                borderRadius: 4,
+                textAlign: 'center',
+                cursor: 'pointer',
+                border: 'none',
+                textDecoration: 'none',
+                margin: 0,
+                display: 'inline-block',
+                transition: 'border-radius linear 0.05s, width linear 0.05s',
+                '&:focus': {
+                    outline: 0
+                }
+            };
         }
-    });
+    }));
 
     const classes = useStyles();
     return <button className={classes.buttonStyles}>Adicionar</button>;
